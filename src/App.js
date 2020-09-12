b799797aae9d1e2f91e7a7034fd11c2da70808ff
Cath11 <Cath11@users.noreply.github.com>
2020-09-12 11:53:01 +0800

    cv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import TodoItem from "./components/TodoItem";
 import TodoForm from "./components/TodoForm";
+import "./App.css";
+
 
 //variables
 
@@ -17,14 +19,21 @@ const addTodo = (text)=> {
   setTodos(newTodos);
 };
 
+
+
+
+
+
 //template
   return ( 
-  <div>
+  <div className="app">
+    <div className="todo-list">
     <h1>My to do list</h1>
     {todos.map((todo, index)=> (
       <TodoItem todo={todo} key={index}/>
     ))}
     <TodoForm addTodo={addTodo}/>
+      </div>
   </div>
   );
 }
